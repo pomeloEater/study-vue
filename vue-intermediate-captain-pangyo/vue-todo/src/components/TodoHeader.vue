7f405402063ae4b709414411b969d1b29628eb2f
jinhui.dev <jinhui.dev@gmail.com>
2022-04-15 17:29:00 +0900

    create 할 일 목록 웹앱 프로젝트 생성

---
+++ vue-intermediate-captain-pangyo/vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,22 @@
+<template>
+<!-- HTML -->
+  <header>
+    <h1>Todo it!</h1>
+  </header>
+</template>
+
+<script>
+/* JavaScript */
+export default {
+
+}
+</script>
+
+<style scoped>
+/* CSS */
+h1 {
+  color: #2f3852;
+  font-weight:900;
+  margin:2.5rem @ 1.5rem
+}
+</style>
