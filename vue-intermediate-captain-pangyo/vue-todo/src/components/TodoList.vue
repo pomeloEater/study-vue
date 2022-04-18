78246f26cd3b31694edadcfb24d9765a7e072dc4
jinhui.dev <jinhui.dev@gmail.com>
2022-04-18 16:28:03 +0900

    modify vuex helper function 적용

--- vue-intermediate-captain-pangyo/vue-todo/src/components/TodoList.vue
+++ vue-intermediate-captain-pangyo/vue-todo/src/components/TodoList.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <transition-group name="list" tag="ul">
-      <li v-for="(todoItem, index) in this.$store.state.todoItems" v-bind:key="index" class="shadow">
+      <li v-for="(todoItem, index) in storedTodoItems" v-bind:key="index" class="shadow">
         <i class="fa-solid fa-check checkBtn" 
           v-bind:class="{checkBtnCompleted: todoItem.completed}"
-          v-on:click="toggleComplete(todoItem, index)"></i>
+          v-on:click="toggleComplete({todoItem, index})"></i>
         <span v-bind:class="{textCompleted: todoItem.completed}">
           {{todoItem.item}}
         </span>
-        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+        <span class="removeBtn" v-on:click="removeTodo({todoItem, index})">
           <i class="fa-solid fa-trash-can"></i>
         </span>
       </li>
@@ -17,16 +17,25 @@
 </template>
 
 <script>
+import {mapGetters, mapMutations } from 'vuex';
+
 export default {
   methods: {
-    removeTodo(todoItem, index) {
-      // this.$emit('removeItem', todoItem, index);
-      this.$store.commit('removeOneItem', {todoItem, index});
-    },
-    toggleComplete(todoItem, index) {
-      // this.$emit('toggleItem', todoItem, index);
-      this.$store.commit('toggleOneItem', {todoItem, index});
-    }
+    ...mapMutations({
+      removeTodo: 'removeOneItem',
+      toggleComplete: 'toggleOneItem'
+    })
+  },
+  computed: {
+    ...mapGetters(['storedTodoItems'])
+    /* 
+      <li v-for="(todoItem, index) in todoItems"
+      와 같이 컴포넌트에 사용되는 메서드 명과 store의 메서도 명이 다른 경우에는
+      ...mapGetters({
+        todoItems: 'storedTodoItems'
+      })
+      와 같이 JSON으로 쓸 수 있다
+    */
   }
 }
 </script>
